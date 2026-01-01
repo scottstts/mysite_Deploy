463c22cf8d6ed3d50dd0652734253549164d818c
scott <st.scott0612@gmail.com>
2026-01-01 00:10:56 +0000

    update project

--- assets/AppsTab-C9vabGGs.js
+++ assets/AppsTab-C9vabGGs.js
@@ -1,4 +1,4 @@
-import{j as e,H as l}from"./index-DSgDVz-C.js";import{G as a}from"./GlassCard-B6FCj_eF.js";import{I as d}from"./ImageSlider-I2gYBPAk.js";import{s as n}from"./safeHtml-CywaOwmt.js";const c=[{id:"ingrelab",appId:1,title:"IngreLab",tagline:"Food Intelligence.",description:`These days we rarely know what is in our food. High sugar, high inflammatory, ultra-processed food has taken up a huge portion of our daily food intake.<br><br>
+import{j as e,H as l}from"./index-Ce9STMF5.js";import{G as a}from"./GlassCard-CFIxcy9K.js";import{I as d}from"./ImageSlider-DwfjVEdp.js";import{s as n}from"./safeHtml-CywaOwmt.js";const c=[{id:"ingrelab",appId:1,title:"IngreLab",tagline:"Food Intelligence.",description:`These days we rarely know what is in our food. High sugar, high inflammatory, ultra-processed food has taken up a huge portion of our daily food intake.<br><br>
 
 IngreLab peels away the fancy packaging and drills down to the ingredients. You just scan the barcode, and it analyzes these health risk factors and builds a risk profile on the ingredients.<br><br>
 
