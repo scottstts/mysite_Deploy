3c0c225b259e5da7ceb077479f0a4e0587bb3549
scott <st.scott0612@gmail.com>
2026-04-27 20:56:04 +0100

    add neon sign animation

--- assets/InspirationsTab-BsIwaAsH.js
+++ assets/InspirationsTab-BsIwaAsH.js
@@ -1,4 +1,4 @@
-import{j as e,H as s}from"./index-DN1w56OZ.js";import{G as t,s as n}from"./safeHtml-BpYG4cGW.js";const i=[{id:"elon",name:"Elon Musk",description:`<span class="font-semibold text-yellow-200">I would pick flawed authenticity over faked perfection any day.</span><br><br>
+import{j as e,H as s}from"./index-DyZp6DgJ.js";import{G as t,s as n}from"./safeHtml-D6YlmHwg.js";const i=[{id:"elon",name:"Elon Musk",description:`<span class="font-semibold text-yellow-200">I would pick flawed authenticity over faked perfection any day.</span><br><br>
 
 To borrow the ending of the <span class="font-semibold italic">Elon Musk</span> biography by Walter Isaacson, "<span class="italic">They can be reckless, cringeworthy, sometimes even toxic. They can also be crazy. Crazy enough to think they can change the world.</span>"<br><br>
 
