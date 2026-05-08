aac7e67a512ae8ca36db96b19c9a2bf515be96c2
scott <st.scott0612@gmail.com>
2026-05-08 17:00:16 +0100

    clamp down DPR with scene detail enrichment

--- assets/AppsTab-SY2thoeb.js
+++ assets/AppsTab-SY2thoeb.js
@@ -1,4 +1,4 @@
-import{j as e,H as l}from"./index-BjHrJqFN.js";import{G as a,s as n}from"./safeHtml-D-8q6lAs.js";import{I as d}from"./ImageSlider-qVCesweR.js";const u=[{id:"jarvis",title:"Jarvis",tagline:"Your AI assistant should be classy.",description:`Jarvis is arguably my favorite AI assistant from any sci-fi stories. He's capable, low key, intelligent, and witty. He is my idea of how a personal AI assistant should be like. Watching Iron Man has been a great inspiration.<br><br>
+import{j as e,H as l}from"./index-1ASonnb2.js";import{G as a,s as n}from"./safeHtml-dBj21bEi.js";import{I as d}from"./ImageSlider-C-jTnuI9.js";const u=[{id:"jarvis",title:"Jarvis",tagline:"Your AI assistant should be classy.",description:`Jarvis is arguably my favorite AI assistant from any sci-fi stories. He's capable, low key, intelligent, and witty. He is my idea of how a personal AI assistant should be like. Watching Iron Man has been a great inspiration.<br><br>
 
 Now we finally have the technology to build this out, or ... close enough.<br><br>
 
--- assets/UnrealBloomPass-Cdtb8U6v.js
+++ assets/UnrealBloomPass-Cdtb8U6v.js
@@ -1,4 +1,4 @@
-import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-D0NHNzwZ.js";import{P as S,F as U}from"./Pass-u3TRXFVW.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-BjHrJqFN.js";import"./ArtInLifeTab.module-BE6fQtgE.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
+import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-DAnMUsMf.js";import{P as S,F as U}from"./Pass-iwlRgY_1.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-1ASonnb2.js";import"./ArtInLifeTab.module-BE6fQtgE.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
