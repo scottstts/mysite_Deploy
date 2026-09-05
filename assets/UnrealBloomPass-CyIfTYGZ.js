9b6098fbff4c54f48d704f89dcad9354b343299b
scott <st.scott0612@gmail.com>
2026-09-05 23:47:30 +0100

    add jelly baby

--- assets/UnrealBloomPass-CyIfTYGZ.js
+++ assets/UnrealBloomPass-CyIfTYGZ.js
@@ -1,4 +1,4 @@
-import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-DLv88awI.js";import{P as S,F as U}from"./Pass-CMyFOL21.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-Bb3rwyGM.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
+import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-BNzZpcp5.js";import{P as S,F as U}from"./Pass-YPQPmKqn.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-D69JaJEE.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
