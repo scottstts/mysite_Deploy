bdcc0f4091e847d29ee096e2c0d0f2c4d4132b21
scott <st.scott0612@gmail.com>
2026-09-08 19:42:24 +0100

    redesign social section

--- assets/UnrealBloomPass-D9XK5nxd.js
+++ assets/UnrealBloomPass-D9XK5nxd.js
@@ -1,4 +1,4 @@
-import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-ClE4HUQL.js";import{P as S,F as U}from"./Pass-CBux4zLX.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-DAzP899e.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
+import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-CQCMkWfF.js";import{P as S,F as U}from"./Pass-BkMAgxjv.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-DxETXx5S.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
