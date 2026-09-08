2becb55acb47e764fcb61bb4190f22e55eb328fa
scott <st.scott0612@gmail.com>
2026-09-08 13:09:29 +0100

    UI adjustments

--- assets/InspirationsTab-Cq-SCVAb.js
+++ assets/InspirationsTab-Cq-SCVAb.js
@@ -1,4 +1,4 @@
-import{j as e,H as s}from"./index-DZ6myqZh.js";import{G as a,s as n}from"./safeHtml-C1uIx2O7.js";const i=[{id:"elon",name:"Elon Musk",description:`<span class="font-semibold text-yellow-200">I would pick flawed authenticity over faked perfection any day.</span><br><br>
+import{j as e,H as s}from"./index-DAzP899e.js";import{G as a,s as n}from"./safeHtml-XF0JELvy.js";const i=[{id:"elon",name:"Elon Musk",description:`<span class="font-semibold text-yellow-200">I would pick flawed authenticity over faked perfection any day.</span><br><br>
 
 To borrow the ending of the <span class="font-semibold italic">Elon Musk</span> biography by Walter Isaacson, "<span class="italic">They can be reckless, cringeworthy, sometimes even toxic. They can also be crazy. Crazy enough to think they can change the world.</span>"<br><br>
 
