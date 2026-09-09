e7caf1e8d9302c86e5137405f7aefdb716ae3855
scott <st.scott0612@gmail.com>
2026-09-09 21:26:23 +0100

    sunsetting imgsherlock

--- assets/InspirationsTab-Dh0rNffy.js
+++ assets/InspirationsTab-Dh0rNffy.js
@@ -1,4 +1,4 @@
-import{j as e,H as s}from"./index-DXB7ZmVV.js";import{G as a,s as n}from"./safeHtml-VQtzwdC5.js";const i=[{id:"elon",name:"Elon Musk",description:`<span class="font-semibold text-yellow-200">I would pick flawed authenticity over faked perfection any day.</span><br><br>
+import{j as e,H as s}from"./index-BqOn6aW7.js";import{G as a,s as n}from"./safeHtml-B5h3PznJ.js";const i=[{id:"elon",name:"Elon Musk",description:`<span class="font-semibold text-yellow-200">I would pick flawed authenticity over faked perfection any day.</span><br><br>
 
 To borrow the ending of the <span class="font-semibold italic">Elon Musk</span> biography by Walter Isaacson, "<span class="italic">They can be reckless, cringeworthy, sometimes even toxic. They can also be crazy. Crazy enough to think they can change the world.</span>"<br><br>
 
