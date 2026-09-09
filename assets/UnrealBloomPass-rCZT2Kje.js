b07dff38ae6071be3c02391d0a77d92ef9c82127
scott <st.scott0612@gmail.com>
2026-09-09 20:37:00 +0100

    update mycraft remaster

--- assets/UnrealBloomPass-rCZT2Kje.js
+++ assets/UnrealBloomPass-rCZT2Kje.js
@@ -1,4 +1,4 @@
-import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-OYN7tLBP.js";import{P as S,F as U}from"./Pass-CkEzD3XC.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-SY6VbEna.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
+import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-CevfryxS.js";import{P as S,F as U}from"./Pass-DPN9zSyr.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-DXB7ZmVV.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
