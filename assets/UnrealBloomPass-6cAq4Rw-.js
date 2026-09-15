0cc8503926007a43c9030b7323e947a0efb8b115
scott <st.scott0612@gmail.com>
2026-09-15 15:03:14 +0100

    yt player aspect ratio auto adjust

--- assets/UnrealBloomPass-6cAq4Rw-.js
+++ assets/UnrealBloomPass-6cAq4Rw-.js
@@ -1,4 +1,4 @@
-import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-InjSzm9g.js";import{P as S,F as U}from"./Pass-Cp0cslX2.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-w32nOLdq.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
+import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-CLi6xiXJ.js";import{P as S,F as U}from"./Pass-CXwXHeuC.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-BYk-fb-D.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
