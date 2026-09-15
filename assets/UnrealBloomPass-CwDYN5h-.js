fac2dc39af4b32a70fee90fae1dca81584341b43
scott <st.scott0612@gmail.com>
2026-09-15 13:58:45 +0100

    bg shutter frozen effect

--- assets/UnrealBloomPass-CwDYN5h-.js
+++ assets/UnrealBloomPass-CwDYN5h-.js
@@ -1,4 +1,4 @@
-import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-Bj8XEgdb.js";import{P as S,F as U}from"./Pass-BaMfqHrS.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-BtNBq_oa.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
+import{C as b,V as u,W as v,H as p,U as T,S as f,a as h,A as M,M as C}from"./ArtInLifeGallery-InjSzm9g.js";import{P as S,F as U}from"./Pass-Cp0cslX2.js";import{C as g}from"./CopyShader-BzTUYzf6.js";import"./index-w32nOLdq.js";import"./ArtInLifeTab.module-CrpLW5Of.js";const _={uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new b(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
